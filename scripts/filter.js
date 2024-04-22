f2b54a2635e2bbf05a0731363e5059eb80a87c7a
Nomio0608 <pnomioko@gmail.com>
2024-04-23 00:34:02 +0800

    data

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,5 +1,28 @@
 // main.js
-import Items from "./Item";
+// import Items from "./Item";
+class Item {
+  constructor(itemObj) {
+    this.artist = itemObj.artist;
+    this.img = itemObj.img;
+    this.type = itemObj.type;
+    this.part = itemObj.part;
+  }
+
+  Render() {
+    return `<article class="makeupPic item">
+       <img src=${this.img} alt="niran" />
+       <h3>Артист ${this.artist}</h3>
+       <div class="star">
+         <i class="fa fa-star checked"></i>
+         <i class="fa fa-star checked"></i>
+         <i class="fa fa-star checked"></i>
+         <i class="fa-regular fa-star"></i>
+         <i class="fa-regular fa-star"></i>
+       </div>
+     </article>`;
+  }
+}
+
 
 const urlParams = new URLSearchParams(document.location.search);
 const typeParam = urlParams.get("type");
@@ -11,7 +34,7 @@ fetch("https://api.jsonbin.io/v3/b/6624ed9bacd3cb34a83c2119")
     return result.json();
   })
   .then((resultObj) => {
-    const rawItems = resultObj.products;
+    const rawItems = resultObj.record.products;
 
     if (typeParam && partParam) {
       filteredMakeup = rawItems.filter(
@@ -24,7 +47,7 @@ fetch("https://api.jsonbin.io/v3/b/6624ed9bacd3cb34a83c2119")
     }
 
     const makeupHTMLArray = filteredMakeup.map((itemObj) => {
-      const item = new Items(itemObj);
+      const item = new Item(itemObj);
       return item.Render();
     });
     const makeupHTML = makeupHTMLArray.reduce(
