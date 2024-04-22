3d8391d37f6058f8c9d51b7ce286c273fc0eb36b
Khangal <118184729+Khangal12@users.noreply.github.com>
2024-04-22 16:41:37 +0800

    Update filter.js

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,5 +1,26 @@
-// main.js
-import Items from "./Item";
+class Items{
+  constructor(itemObj) {
+    this.artist = itemObj.artist;
+    this.img = itemObj.img;
+    this.type = itemObj.type;
+    this.part = itemObj.part;
+  }
+
+  Render() {
+    return `<article class="makeupPic item">
+       <img src=${this.img} alt="niran" />
+       <h3>Артист ${this.artist}</h3>
+       <div class="star">
+         <i class="fa fa-star checked"></i>
+         <i class="fa fa-star checked"></i>
+         <i class="fa fa-star checked"></i>
+         <i class="fa-regular fa-star"></i>
+         <i class="fa-regular fa-star"></i>
+       </div>
+     </article>`;
+  }
+}
+
 
 const urlParams = new URLSearchParams(document.location.search);
 const typeParam = urlParams.get("type");
@@ -11,7 +32,7 @@ fetch("https://api.jsonbin.io/v3/b/6624ed9bacd3cb34a83c2119")
     return result.json();
   })
   .then((resultObj) => {
-    const rawItems = resultObj.products;
+    const rawItems = resultObj.record.products;
 
     if (typeParam && partParam) {
       filteredMakeup = rawItems.filter(
